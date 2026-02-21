9eb0d4fdb2f1a5579e87c617d52e26d68759aabc
myfish-code <125704144+myfish-code@users.noreply.github.com>
2026-02-21 16:56:01 +0100

    refactor something

--- Lcoin/urls.py
+++ Lcoin/urls.py
@@ -17,10 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+from django.conf import settings             
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('Lcoin.api_urls')),
 
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
     re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]

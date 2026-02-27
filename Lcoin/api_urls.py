ca7fd905e012b6c5f2e6b637856c7eaa6c89f976
myfish-code <125704144+myfish-code@users.noreply.github.com>
2026-02-27 22:40:22 +0100

    Delete Lcoin directory

--- Lcoin/api_urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('users/', include('users.api_urls')),
-    path('homework/', include('homework.api_urls')),
-    path('chat/', include('chat.api_urls')),
-]

e341b64949cb58563d08fd45fb95a2f47f068060
myfish-code <125704144+myfish-code@users.noreply.github.com>
2026-03-01 18:22:55 +0100

    add test

--- backend/Lcoin/settings.py
+++ backend/Lcoin/settings.py
@@ -31,7 +31,6 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['127.0.0.1', 'localhost'])
 
-
 # Application definition
 
 INSTALLED_APPS = [
